afeda5536a81fe991a6874cac9a7b4eea74e38b7
JeffreyZhu <1624410543@qq.com>
2024-11-23 17:09:48 +0800

    update

--- jeffreysblogfronted/src/app/layout.tsx
+++ jeffreysblogfronted/src/app/layout.tsx
@@ -2,12 +2,13 @@
  * @Author: JeffreyZhu 1624410543@qq.com
  * @Date: 2024-10-30 20:50:44
  * @LastEditors: JeffreyZhu 1624410543@qq.com
- * @LastEditTime: 2024-11-18 13:07:06
+ * @LastEditTime: 2024-11-18 15:57:39
  * @FilePath: \JeffreysBlog\jeffreysblogfronted\src\app\layout.tsx
  * @Description: File Description Here...
  * 
  * Copyright (c) 2024 by JeffreyZhu, All Rights Reserved. 
  */
+import NavigateBar from "./componets/NavigateBar";
 import "./globals.css";
 
 
@@ -20,7 +21,8 @@ export default function RootLayout({
     <html lang="en">
       <body
       >
-        {children}
+        <NavigateBar></NavigateBar>
+        <div>{children}</div>
       </body>
     </html>
   );
