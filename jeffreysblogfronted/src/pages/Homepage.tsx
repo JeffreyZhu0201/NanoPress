fdd44ce76636b039644cc4ce252499c2e1b91a21
JeffreyZhu <1624410543@qq.com>
2024-11-26 15:11:54 +0800

    intro

--- jeffreysblogfronted/src/pages/Homepage.tsx
+++ jeffreysblogfronted/src/pages/Homepage.tsx
@@ -2,7 +2,7 @@
  * @Author: JeffreyZhu 1624410543@qq.com
  * @Date: 2024-11-25 20:51:21
  * @LastEditors: JeffreyZhu 1624410543@qq.com
- * @LastEditTime: 2024-11-26 14:46:06
+ * @LastEditTime: 2024-11-26 15:02:10
  * @FilePath: \JeffreysBlog\jeffreysblogfronted\src\pages\Homepage.tsx
  * @Description: File Description Here...
  * 
@@ -17,11 +17,18 @@ function Homepage(){
 
 
     return (
-        <div className="Homepage bg-slate-400 w-full flex p-4 md:p-10">
+        <div className="Homepage bg-slate-400 w-full flex p-2 md:p-14">
             <div className="introduction">
-                <img src={Avatar} alt="" className="border-2 md:border-4 h-14 md:h-20 border-emerald-100 md:border-emerald-300 my-6 md:my-8 rounded-full"/>
-                <p className="text-3xl font-bold md:text-4xl my-6 md:my-8">Hi,I am Jeffrey Zhu</p>
-                <p className="text-xl font-sans my-6 md:my-8 ">A software engineer and open-source advocate enjoying the sunny life in Barcelona, Spain.</p>
+                <img src={Avatar} alt="" className="border-2 md:border-4 h-14 md:h-20 border-emerald-100 md:border-emerald-300 my-6 rounded-full"/>
+                <p className="text-3xl font-bold md:text-4xl my-6">Hi,I am Jeffrey Zhu</p>
+                <p className="text-xl font-sans my-6 ">A software engineer and open-source advocate enjoying the sunny life in Barcelona, Spain.</p>
+
+                <div className="flex h-12 md:w-36 w-full text-2xl font-bold text-gray-200
+                rounded-sm items-center justify-center bg-green-500 hover:bg-green-800 ease-in-out transition-colors hover:cursor-pointer
+                ">
+                    <div>Say Hello!</div>
+                </div>
+
             </div>
             <div>
 
