b0c51578b012f1ff86f233eb56221de886d065dd
JeffreyZhu <1624410543@qq.com>
2024-11-28 19:11:16 +0800

    添加博客页面

--- jeffreysblogfronted/src/pages/Blogs.tsx
+++ jeffreysblogfronted/src/pages/Blogs.tsx
@@ -2,7 +2,7 @@
  * @Author: JeffreyZhu 1624410543@qq.com
  * @Date: 2024-11-25 21:03:31
  * @LastEditors: JeffreyZhu 1624410543@qq.com
- * @LastEditTime: 2024-11-25 21:03:31
+ * @LastEditTime: 2024-11-28 19:01:26
  * @FilePath: \JeffreysBlog\jeffreysblogfronted\src\pages\Blogs.tsx
  * @Description: File Description Here...
  * 
@@ -10,12 +10,19 @@
  */
 
 import React from "react"
+import ArticleCard from "../components/ArticleCard"
 
 function Blogs() {
     
     return (
-        <div>
-            i'm blogs
+        <div className="p-2 md:p-16 bg-slate-700">
+            <div className="mb-2 md:mb-16">
+                <h1 className="font-bold text-xl md:text-4xl text-white mt-2 mb-8">Blogs</h1>
+                <p className="text-base md:text-lg text-white overflow-ellipsis">Articles, tutorials, snippets, rants, and everything else. Subscribe for updates as they happen.</p>
+            </div>
+            <div>
+                <ArticleCard title={"123"} read={"123"} date={"123"} category={[{name:"123",color:"red"}]}></ArticleCard>
+            </div>
         </div>
     )
 }
--- jeffreysblogfronted/src/pages/Projects.tsx
+++ jeffreysblogfronted/src/pages/Projects.tsx
@@ -2,7 +2,7 @@
  * @Author: JeffreyZhu 1624410543@qq.com
  * @Date: 2024-11-25 21:04:39
  * @LastEditors: JeffreyZhu 1624410543@qq.com
- * @LastEditTime: 2024-11-25 21:10:04
+ * @LastEditTime: 2024-11-28 19:07:23
  * @FilePath: \JeffreysBlog\jeffreysblogfronted\src\pages\Projects.tsx
  * @Description: File Description Here...
  * 
@@ -10,16 +10,22 @@
  */
 
 import React from "react"
+import ProjectCard from "../components/ProjectCard"
 
-function Projects(){
 
-
+function Projects() {
+    
     return (
-        <div>
-            I'm projects
+        <div className="p-2 md:p-16 bg-slate-700">
+            <div className="mb-2 md:mb-16">
+                <h1 className="font-bold text-xl md:text-4xl text-white mt-2 mb-8">Projects</h1>
+                <p className="text-base md:text-lg text-white overflow-ellipsis">Articles, tutorials, snippets, rants, and everything else. Subscribe for updates as they happen.</p>
+            </div>
+            <div>
+                <ProjectCard title={"I'm title"} intro={"Introduction"} date={"im'date"}></ProjectCard>      
+            </div>
         </div>
     )
-
 }
 
 export default Projects
