6e498b6c81ae8a19c80ee17d4bf3aced112d7839
JeffreyZhu <1624410543@qq.com>
2024-12-01 00:34:51 +0800

    update

--- jeffreysblogfronted/src/pages/Blogs.tsx
+++ jeffreysblogfronted/src/pages/Blogs.tsx
@@ -2,7 +2,7 @@
  * @Author: JeffreyZhu 1624410543@qq.com
  * @Date: 2024-11-25 21:03:31
  * @LastEditors: JeffreyZhu 1624410543@qq.com
- * @LastEditTime: 2024-12-01 00:27:35
+ * @LastEditTime: 2024-12-01 00:34:45
  * @FilePath: \JeffreysBlog\jeffreysblogfronted\src\pages\Blogs.tsx
  * @Description: File Description Here...
  * 
@@ -19,7 +19,7 @@ function Blogs() {
 
     const [loading, setLoading] = useState(false);
     const [currentPage, setCurrentPage] = useState(1);
-
+    console.log(loading)
     const [blogsPerPage] = useState(5);
 
     useEffect(() => {
