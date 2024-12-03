4107a0c94aadfb4b7866c799e5ddfe0488e3f8c5
JeffreyZhu <1624410543@qq.com>
2024-12-03 23:20:38 +0800

    放弃实体类里包对象

--- jeffreysblogbackend/src/main/java/com/jeffrey/jeffreysblog/entity/Author.java
+++
@@ -1,6 +0,0 @@
-package com.jeffrey.jeffreysblog.entity;
-
-public class Author {
-    String authorId;
-    String name;
-}
--- jeffreysblogbackend/src/main/java/com/jeffrey/jeffreysblog/entity/Category.java
+++
@@ -1,6 +0,0 @@
-package com.jeffrey.jeffreysblog.entity;
-
-public class Category {
-    private String name;
-    private String color;
-}
