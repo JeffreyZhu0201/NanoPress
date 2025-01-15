ea963dfa09b9fb76c6357c5e42e231af774c07d5
Jeffrey Zhu <1624410543@qq.com>
2025-01-15 17:01:40 +0800

    added category module

---
+++ jeffreysblogbackend/src/main/java/com/jeffrey/jeffreysblog/mapper/CategoryMapper.java
@@ -0,0 +1,16 @@
+package com.jeffrey.jeffreysblog.mapper;
+
+import com.jeffrey.jeffreysblog.entity.Category;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+import org.apache.ibatis.annotations.*;
+
+@Mapper
+@Repository
+public interface CategoryMapper {
+    Boolean addCategory(@Param("category") Category category);
+    Boolean deleteCategory(String categoryId);
+    Boolean updateCategory(@Param("category") Category category);
+    Category getCategoryById(String categoryId);
+}
---
+++ jeffreysblogbackend/src/main/java/com/jeffrey/jeffreysblog/service/serviceImpl/CategoryServiceImpl.java
@@ -0,0 +1,52 @@
+package com.jeffrey.jeffreysblog.service.serviceImpl;
+
+import com.jeffrey.jeffreysblog.mapper.CategoryMapper;
+import com.jeffrey.jeffreysblog.service.CategoryService;
+
+import com.jeffrey.jeffreysblog.common.Result;
+import com.jeffrey.jeffreysblog.entity.Category;
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    @Resource
+    private CategoryMapper categoryMapper;
+
+    @Override
+    public Result addCategory(Category category) {
+        category.setCategoryId(String.valueOf(UUID.randomUUID()));
+        if(categoryMapper.addCategory(category)){
+            return Result.success("200","success",category);
+        }
+        return Result.error("403","add failed",null);
+    }
+
+    @Override
+    public Result deleteCategory(String categoryId) {
+        if(categoryMapper.deleteCategory(categoryId)){
+            return Result.success("200","success",null);
+        }
+        return Result.error("403","delete failed",null);
+    }
+
+    @Override
+    public Result updateCategory(Category category) {
+        if(categoryMapper.updateCategory(category)){
+            return Result.success("200","success",category);
+        }
+        return Result.error("403","update failed",null);
+    }
+
+    @Override
+    public Result getCategoryById(String categoryId) {
+        Category category = categoryMapper.getCategoryById(categoryId);
+        if(category != null){
+            return Result.success("200","success",category);
+        }
+        return Result.error("403","update failed",null);
+    }
+}
